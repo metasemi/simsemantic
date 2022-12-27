bd5f90a5b3b0d75232608e04324422f923980112
metasemi <mike@michael-brian-orr.net>
2022-12-27 11:22:45 -0800

    make Trajectory attrs private, add read-only props for some

--- src/simsemantics/trajectory.py
+++ src/simsemantics/trajectory.py
@@ -16,7 +16,7 @@ class TrajectoryException(Exception):
 
 
 class Trajectory:
-    """Semantic trajectory of a text string. Construct, calculate once, then use attributes read-only."""
+    """Semantic trajectory of a text string. Construct, calculate once, then use read-only properties."""
     def __init__(self, text, engine=DEFAULT_ENGINE,
                  api_key=DEFAULT_OPENAI_API_KEY, tokenizer=transformers.GPT2TokenizerFast.from_pretrained('gpt2')):
         """
@@ -27,45 +27,60 @@ class Trajectory:
         api_key     --  OpenAI api key (default OPENAI_API_KEY os environment variable)
         tokenizer   --  Hugging Face fast transformer (default GPT2TokenizerFast)
 
-        Additional attributes:
-
+        Calculated properties:
         encoding    --  encoding produced by tokenizer
         ends        --  list of indexes (in original text) one past each token
         delta_mus   --  list per token, delta between semantic embedding of prev token (init zeros) and curr token
         """
-        self.text = text
-        self.engine = engine
-        self.api_key = api_key
-        self.tokenizer = tokenizer
+        self._text = text
+        self._engine = engine
+        self._api_key = api_key
+        self._tokenizer = tokenizer
 
-        self.encoding = None
-        self.ends = None
+        self._encoding = None
+        self._ends = None
+        self._delta_mus = []
 
-        self.delta_mus = []
+    @property
+    def text(self):
+        return self._text
+
+    @property
+    def encoding(self):
+        return self._encoding
+
+    @property
+    def ends(self):
+        return self._ends
+
+    @property
+    def delta_mus(self):
+        return self._delta_mus
 
     def calculate(self):
-        """Compute encoding, ends, and delta_mus."""
-        if self.encoding is not None:
+        """Compute the encoding, ends, and delta_mus."""
+        if self._encoding is not None:
             raise TrajectoryException(f'{self.__repr__()} was already calculated.')
-        if self.tokenizer.is_fast is False:
+        if self._tokenizer.is_fast is False:
             raise TrajectoryException(f'{self.__repr__()} requires a Hugging Face fast tokenizer.')
 
-        self.encoding = self.tokenizer(self.text)
-        self.ends = [self.encoding.token_to_chars(i)[1] for i, _ in enumerate(self.encoding['input_ids'])]
+        self._encoding = self._tokenizer(self._text)
+        self._ends = [self._encoding.token_to_chars(i)[1] for i, _ in enumerate(self._encoding['input_ids'])]
 
         mu_prev = None
-        for end in self.ends:
-            state = self.text[:end]
-            mu = np.array(openai.Embedding.create(input=[state], engine=self.engine)['data'][0]['embedding'])
+        for end in self._ends:
+            state = self._text[:end]
+            mu = np.array(openai.Embedding.create(input=[state], engine=self._engine)['data'][0]['embedding'])
             if mu_prev is None:
                 mu_prev = np.zeros(len(mu))
-            self.delta_mus.append(mu - mu_prev)
+            self._delta_mus.append(mu - mu_prev)
             mu_prev = mu
 
 
 if __name__ == '__main__':
 
     print()
+
     st = Trajectory('By default this will be indoors at my house. (We could consider moving outside, but only '
                     'if the weather forecast improves: currently the temp is expected to be barely above '
                     'freezing.) Re Covid/flu, I’m going to open the bidding with a suggestion of masks '
@@ -89,7 +104,8 @@ if __name__ == '__main__':
         sum_of_delta_mus = sum_of_delta_mus + delta_mu
     print('Sum of delta_mus, vector length:', len(sum_of_delta_mus))
     print('Sum of delta_mus [:4] ... [-4:]:', sum_of_delta_mus[:4], '...', sum_of_delta_mus[-4:])
-    mu_endstate = np.array(openai.Embedding.create(input=[st.text], engine=st.engine)['data'][0]['embedding'])
+    resp = openai.Embedding.create(input=[st.text], engine=st._engine)  # note private attr access for testing
+    mu_endstate = np.array(resp['data'][0]['embedding'])
     print('mu(end state), vector length:', len(mu_endstate))
     print('mu(end state) [:4] ... [-4:]:', mu_endstate[:4], '...', mu_endstate[-4:])
     print('sum_of_delta_mus and mu(end state) effectively equal?', np.allclose(sum_of_delta_mus, mu_endstate))
