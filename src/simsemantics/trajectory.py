0199cd590eb42c870f04148e00e25bb58e0f3e44
metasemi <mike@michael-brian-orr.net>
2023-01-09 09:41:15 -0800

    enhance pytests, including significant changes to cope with noisiness of current OpenAI semantic embedding

--- src/simsemantics/trajectory.py
+++ src/simsemantics/trajectory.py
@@ -17,20 +17,22 @@ class TrajectoryException(Exception):
 
 class Trajectory:
     """Semantic trajectory of a text string. Construct, calculate once, then use read-only properties."""
+
     def __init__(self, text, engine=DEFAULT_ENGINE,
                  api_key=DEFAULT_OPENAI_API_KEY, tokenizer=transformers.GPT2TokenizerFast.from_pretrained('gpt2')):
         """
         Construct semantic trajectory object. Call calculate() before using attributes other than init parameters.
 
-        Keyword arguments:
+        Arguments:
+        text        --  String whose semantic trajectory is to be represented; eg a prompt/completion concatenation
         engine      --  OpenAI embedding model by name (default 'text-embedding-ada-002')
-        api_key     --  OpenAI api key (default OPENAI_API_KEY os environment variable)
-        tokenizer   --  Hugging Face fast transformer (default GPT2TokenizerFast)
+        api_key     --  OpenAI api key (default OPENAI_API_KEY os environment variable's value)
+        tokenizer   --  Hugging Face fast tokenizer (default GPT2TokenizerFast)
 
         Calculated properties:
         encoding    --  encoding produced by tokenizer
         ends        --  list of indexes (in original text) one past each token
-        delta_mus   --  list per token, delta between semantic embedding of prev token (init zeros) and curr token
+        delta_mus   --  numpy array: per token, per semantic embedding dimension, delta from embed val at prev token
         """
         self._text = text
         self._engine = engine
@@ -39,7 +41,7 @@ class Trajectory:
 
         self._encoding = None
         self._ends = None
-        self._delta_mus = []
+        self._delta_mus = None
 
     @property
     def text(self):
@@ -60,18 +62,251 @@ class Trajectory:
     def calculate(self):
         """Compute the encoding, ends, and delta_mus."""
         if self._encoding is not None:
-            raise TrajectoryException(f'{self.__repr__()} was already calculated.')
+            raise TrajectoryException(f'{type(self)} was already calculated.')
         if self._tokenizer.is_fast is False:
-            raise TrajectoryException(f'{self.__repr__()} requires a Hugging Face fast tokenizer.')
+            raise TrajectoryException(f'{type(self)} requires a Hugging Face fast tokenizer.')
 
         self._encoding = self._tokenizer(self._text)
         self._ends = [self._encoding.token_to_chars(i)[1] for i, _ in enumerate(self._encoding['input_ids'])]
 
         mu_prev = None
+        delta_mus = []
         for end in self._ends:
             state = self._text[:end]
             mu = np.array(openai.Embedding.create(input=[state], engine=self._engine)['data'][0]['embedding'])
             if mu_prev is None:
                 mu_prev = np.zeros(len(mu))
-            self._delta_mus.append(mu - mu_prev)
+            delta_mus.append(mu - mu_prev)
             mu_prev = mu
+        self._delta_mus = np.array(delta_mus)
+
+
+if __name__ == '__main__':
+    """
+    Run this to explore operation of the Trajectory class and collect information for test_Trajectory.py.
+
+    In addition to producing useful console output, this code captures reference values for semantic embedding tests,
+    allows them to be sanity-checked, and proposes tolerances for comparing test results to expected results.
+
+    The following code automatically pickles ref_text, ref_first/last_six_tokens, ref_delta_mus, and ref_tolerances
+    into the current directory. The resulting files must be manually copied into the tests directory to become
+    effective.
+
+    ref_criterion_embeddings_match must be manually copied into test source code to become effective.
+    """
+    import pickle
+
+    ref_text = ('Remarkably—and also, perhaps, trivially—the relationship between succinct expressibility and depth '
+                'of pattern that we see in 64k Intros provably holds for any informational, cognitive, or semiotic '
+                'system.')
+    ref_first_six_tokens = None
+    ref_last_six_tokens = None
+    ref_delta_mus = None
+    ref_tolerances = None
+
+    def ref_criterion_embeddings_match(out_of_tolerance_delta_mus_percentage, worst_out_of_tolerance_ratio):
+        return out_of_tolerance_delta_mus_percentage < 0.6 and worst_out_of_tolerance_ratio < 5.
+
+    nb_ref_trajectories = 64
+    nb_test_trajectories = 8
+
+    print()
+    print(f'creating {nb_ref_trajectories} reference trajectories')
+    print('--------------------------------')
+    sts = []
+    for _ in range(nb_ref_trajectories):
+        st = Trajectory(ref_text)
+        sts.append(st)
+    print('len(sts):   ', len(sts))
+    print('sts[0].text:   ', sts[0].text[:64], '...', sts[0].text[-32:])
+    print('sts[-1].text:   ', sts[-1].text[:64], '...', sts[-1].text[-32:])
+
+    print()
+    print('calculating ref trajectories', end='', flush=True)
+    for st in sts:
+        print('.', end='', flush=True)
+        st.calculate()
+    print(' Done!')
+
+    print()
+    tx = sts[0].text
+    ends = sts[0].ends
+    ref_first_six_tokens = [tx[:ends[0]]]
+    for tk in range(1, 6):
+        ref_first_six_tokens.append(tx[ends[tk - 1]:ends[tk]])
+    ref_last_six_tokens = []
+    for tk in range(-6, 0):
+        ref_last_six_tokens.append(tx[ends[tk - 1]:ends[tk]])
+    print('first tokens:    ', ref_first_six_tokens)
+    print('last tokens:       ', ref_last_six_tokens)
+
+    sms = np.array([traj.delta_mus for traj in sts])
+    print('sms.shape:   ', sms.shape)
+    sms_abs = np.absolute(sms)
+    sms_abs_diffs = sms_abs.max(axis=0) - sms_abs.min(axis=0)
+    print('sms.min():   ', sms.min())
+    print('sms.max():   ', sms.max())
+    print('sms.mean():   ', sms.mean())
+    print('sms_abs.min():   ', sms_abs.min())
+    print('sms_abs.max():   ', sms_abs.max())
+    print('sms_abs.mean():   ', sms_abs.mean())
+    print('sms_abs_diffs.min():   ', sms_abs_diffs.min())
+    print('sms_abs_diffs.max():   ', sms_abs_diffs.max())
+    print('sms_abs_diffs.mean():   ', sms_abs_diffs.mean())
+    print('--------------------------------')
+    stds = sms.std(axis=0)
+    print('stds.shape:   ', stds.shape)
+    print('standard deviations min:   ', stds.min())
+    print('standard deviations max:   ', stds.max())
+    print('standard deviations mean:   ', stds.mean())
+
+    print()
+    print('calculating reference delta_mus')
+    print('--------------------------------')
+    ref_delta_mus = sms.mean(axis=0)
+    print('ref_delta_mus.shape:   ', ref_delta_mus.shape)
+    dms = ref_delta_mus
+    dms_abs = np.absolute(dms)
+    print('dms.min():   ', dms.min())
+    print('dms.max():   ', dms.max())
+    print('dms.mean():   ', dms.mean())
+    print('dms_abs.min():   ', dms_abs.min())
+    print('dms_abs.max():   ', dms_abs.max())
+    print('dms_abs.mean():   ', dms_abs.mean())
+    print('--------------------------------')
+    descending_abs_dms = sorted(dms_abs.flatten(), reverse=True)
+    start = 0
+    while start <= len(descending_abs_dms) - 8:
+        print(f'descending abs delta mus, [{start} : {start + 8}]:   ', descending_abs_dms[start:start + 8])
+        start += len(descending_abs_dms) // 8
+
+    print()
+    print('calculating reference tolerances')
+    print('--------------------------------')
+    ref_tolerances = np.zeros(dms.shape)
+    for t, _ in enumerate(ref_tolerances):
+        for feat, _ in enumerate(ref_tolerances[t]):
+            tolerance = 3. * stds[t][feat]
+            tolerance = max(tolerance, min(0.025, abs(dms[t][feat] / 3.)))
+            tolerance = max(tolerance, 0.001)
+            ref_tolerances[t][feat] = tolerance
+    descending_tolerances = sorted(ref_tolerances.flatten(), reverse=True)
+    start = 0
+    while start <= len(descending_tolerances) - 8:
+        print(f'descending ref tolerances, [{start} : {start + 8}]:   ', descending_tolerances[start:start + 8])
+        start += len(descending_tolerances) // 8
+
+    print()
+    print('pickling ref objects to current directory files (except ref_criterion_embeddings_match)')
+    print('to make the resulting ref_*.pickle files authoritative, copy them to the tests directory')
+    print('--------------------------------')
+    ref_text_fn = 'ref_text.pickle'
+    ref_first_six_tokens_fn = 'ref_first_six_tokens.pickle'
+    ref_last_six_tokens_fn = 'ref_last_six_tokens.pickle'
+    ref_delta_mus_fn = 'ref_delta_mus.pickle'
+    ref_tolerances_fn = 'ref_tolerances.pickle'
+
+    # remove older pickles, so they don't cause confusion if something goes wrong with creating the new ones
+    def remove_if_exists(fn):
+        try:
+            os.remove(fn)
+        except FileNotFoundError:
+            pass
+    remove_if_exists(ref_text_fn)
+    remove_if_exists(ref_first_six_tokens_fn)
+    remove_if_exists(ref_last_six_tokens_fn)
+    remove_if_exists(ref_delta_mus_fn)
+    remove_if_exists(ref_tolerances_fn)
+
+    print('pickling ref_text as ./ref_text.pickle')
+    with open(ref_text_fn, 'wb') as f:
+        pickle.dump(ref_text, f)
+    print('pickling ref_first_six_tokens as ./ref_first_six_tokens.pickle')
+    with open(ref_first_six_tokens_fn, 'wb') as f:
+        pickle.dump(ref_first_six_tokens, f)
+    print('pickling ref_last_six_tokens as ./ref_last_six_tokens.pickle')
+    with open(ref_last_six_tokens_fn, 'wb') as f:
+        pickle.dump(ref_last_six_tokens, f)
+    print('pickling ref_delta_mus as ./ref_delta_mus.pickle')
+    with open(ref_delta_mus_fn, 'wb') as f:
+        pickle.dump(ref_delta_mus, f)
+    print('pickling ref_tolerances as ./ref_tolerances.pickle')
+    with open(ref_tolerances_fn, 'wb') as f:
+        pickle.dump(ref_tolerances, f)
+
+    print()
+    print(f'creating and calculating {nb_test_trajectories} test trajectories', end='', flush=True)
+    test_traj = []
+    for tt in range(nb_test_trajectories):
+        print('.', end='', flush=True)
+        test_traj.append(Trajectory(ref_text))
+        test_traj[tt].calculate()
+    print(' Done!')
+
+    tfeatmins = []
+    tmins = []
+    tfeatmaxes = []
+    tmaxes = []
+    tfeatmeans = []
+    tmeans = []
+    tfeatminabs = []
+    tminabs = []
+    trefdiffs = []
+    failures = 0
+    for att, ttraj in enumerate(test_traj):
+        print()
+        print(f'checking test trajectory {att} vs tolerances...')
+        print('--------------------------------')
+        tdms = ttraj.delta_mus
+        tfeatmins.append(tdms.min(axis=0, initial=np.inf))
+        tmins.append(tfeatmins[att].min())
+        tfeatmaxes.append(tdms.max(axis=0, initial=-np.inf))
+        tmaxes.append(tfeatmaxes[att].max())
+        tfeatmeans.append(tdms.mean(axis=0))
+        tmeans.append(tdms.mean())
+        tfeatminabs.append(np.absolute(tdms).min(axis=0, initial=np.inf))
+        tminabs.append(tfeatminabs[att].min())
+        trefdiffs.append(np.absolute(tdms - dms))
+        print(f'delta_mus min:{tmins[att]} max:{tmaxes[att]} mean:{tmeans[att]} minabs:{tminabs[att]}')
+        print(f'ref diffs min:{trefdiffs[att].min()} max:{trefdiffs[att].max()} mean:{trefdiffs[att].mean()}')
+
+        misfit_count = 0
+        worst_misfit = 0.
+        intolerables = []
+        for t, _ in enumerate(ref_tolerances):
+            intolerables.append([])
+            for feat, _ in enumerate(ref_tolerances[t]):
+                misfit = trefdiffs[att][t][feat] / ref_tolerances[t][feat]
+                if misfit > worst_misfit:
+                    worst_misfit = misfit
+                if misfit > 1:
+                    misfit_count += 1
+                    intolerables[t].append((misfit, feat))
+
+        misfit_percentage = misfit_count * 100. / ref_tolerances.size
+        print('misfit percentage:   ', misfit_percentage)
+        print('worst misfit:   ', worst_misfit)
+        suggested_verdict = 'PASS'
+        if ref_criterion_embeddings_match(misfit_percentage, worst_misfit) is False:
+            suggested_verdict = 'FAIL'
+            failures += 1
+        print('suggested_verdict:   ', suggested_verdict)
+        print('out-of-tolerance delta_mus:')
+        if worst_misfit <= 1:
+            print('  none found')
+        else:
+            for t, intols in enumerate(intolerables):
+                if len(intols) > 0:
+                    print(f'  at token {t}')
+                    for intol in sorted(intols, reverse=True)[:3]:
+                        ratio, ft = intol
+                        print(f'      feature {ft} is out of tolerance')
+                        print(f'          miss ratio: {ratio} ({trefdiffs[att][t][ft]}/{ref_tolerances[t][ft]})')
+                        print(f'          test delta mu: {tdms[t][ft]} ref delta mu: {dms[t][ft]} std: {stds[t][ft]}')
+                    if len(intols) > 3:
+                        print(f'      ...and {len(intols) - 3} more features are out of tolerance')
+
+    print()
+    print(f'test trajectory summary')
+    print('--------------------------------')
+    print(f'{failures} of {len(test_traj)} test trajectories failed ref criterion')
--- tests/test_Trajectory.py
+++ tests/test_Trajectory.py
@@ -1,28 +1,53 @@
-"""test Trajectory class. For now all constructor kwargs are defaulted."""
+"""test Trajectory class. For now, all constructor kwargs are defaulted (and the test code relies on this)."""
+
+'''
+NOTE: Some tests compare semantic embedding results to expected values. Because current OpenAI embedding results are
+quite noisy, and because the embedding models themselves evolve over time, it's challenging to test that embedding code
+is working correctly. A lot of attention is required to create and maintain effective comparisons, and even then
+occasional one-off test failures are to be expected. All this is worth it to catch real coding errors that affect
+embedding results unintentionally.
+'''
 
 import pytest
 from simsemantics import Trajectory, TrajectoryException
 import numpy as np
 import openai
+import pickle
 
-some_text = ('By default this will be indoors at my house. (We could consider moving outside, but only '
-             'if the weather forecast improves: currently the temp is expected to be barely above '
-             'freezing.) Re Covid/flu, I’m going to open the bidding with a suggestion of masks '
-             'optional, but if anyone would prefer masks required or Zoom, please don’t hesitate to '
-             'chime in. We can talk about this, and my vote will be for the most conservative protocol '
-             'wanted by anyone.')
+'''
+reference objects: except for ref_criterion_embeddings_match, the values of these reference objects are normally
+pickled by trajectory.py run as a standalone script. ref_criterion_embeddings_match is normally copied from
+trajectory.py's source code.
+
+IMPORTANT: the files containing the pickled values must be MANUALLY COPIED into the tests directory once deemed good.
+'''
+
+with open('ref_text.pickle', 'rb') as f:
+    ref_text = pickle.load(f)
+with open('ref_first_six_tokens.pickle', 'rb') as f:
+    ref_first_six_tokens = pickle.load(f)
+with open('ref_last_six_tokens.pickle', 'rb') as f:
+    ref_last_six_tokens = pickle.load(f)
+with open('ref_delta_mus.pickle', 'rb') as f:
+    ref_delta_mus = pickle.load(f)
+with open('ref_tolerances.pickle', 'rb') as f:
+    ref_tolerances = pickle.load(f)
+
+
+def ref_criterion_embeddings_match(out_of_tolerance_delta_mus_percentage, worst_out_of_tolerance_ratio):
+    return out_of_tolerance_delta_mus_percentage < 0.6 and worst_out_of_tolerance_ratio < 5.
 
 
 def test_construct():
-    st = Trajectory(some_text)
-    assert st.text == some_text
+    st = Trajectory(ref_text)
+    assert st.text == ref_text
     assert st.encoding is None
     assert st.ends is None
-    assert st._delta_mus == []
+    assert st._delta_mus == None
 
 
 def test_hidden_properties():
-    st = Trajectory(some_text)
+    st = Trajectory(ref_text)
     with pytest.raises(AttributeError):
         x = st.engine
     with pytest.raises(AttributeError):
@@ -32,7 +57,7 @@ def test_hidden_properties():
 
 
 def test_readonly_properties():
-    st = Trajectory(some_text)
+    st = Trajectory(ref_text)
     with pytest.raises(AttributeError):
         st.text = 'something'
     with pytest.raises(AttributeError):
@@ -44,48 +69,75 @@ def test_readonly_properties():
 
 
 def test_calculate():
-    st = Trajectory(some_text)
+    st = Trajectory(ref_text)
     st.calculate()
-    assert st.text == some_text
+    assert st.text == ref_text
     assert st.encoding is not None
     assert st.ends is not None
-    assert len(st.delta_mus) > 0
+    assert st.delta_mus is not None
 
 
 def test_calculated_properties():
-    st = Trajectory(some_text)
+    st = Trajectory(ref_text)
     st.calculate()
-    assert len(st.encoding['input_ids']) == 99
-    assert len(st.ends) == 99
-    assert len(st.delta_mus) == 99
-    assert len(st.delta_mus[0]) == 1536
-    assert len(st.delta_mus[-1]) == 1536
-    assert st.text[0:st.ends[0]] == 'By'
-    assert st.text[st.ends[0]:st.ends[1]] == ' default'
-    assert st.text[st.ends[1]:st.ends[2]] == ' this'
-    assert st.text[st.ends[2]:st.ends[3]] == ' will'
-    assert st.text[st.ends[-3]:st.ends[-2]] == ' anyone'
-    assert st.text[st.ends[-2]:st.ends[-1]] == '.'
-    assert np.allclose(st.delta_mus[0][:4], [-0.01663603, -0.01701961, 0.00538078, -0.0021239], rtol=1e-02)
-    assert np.allclose(st.delta_mus[0][-4:], [0.00093853, -0.01335428, -0.02096906, -0.01966205], rtol=1e-02)
-    assert np.allclose(st.delta_mus[-1][:4], [-0.00184096, -0.00165311, 0.00118624, -0.00252204], rtol=1e-02)
-    assert np.allclose(st.delta_mus[-1][-4:], [-0.00203804, 0.00442898, -0.00060725, -0.00459949], rtol=1e-02)
+    assert len(st.encoding['input_ids']) == len(ref_delta_mus)
+    assert len(st.ends) == len(ref_delta_mus)
+    assert st.ends[-1] == len(st.text)
+    assert st.text[0:st.ends[0]] == ref_first_six_tokens[0]
+    assert st.text[st.ends[0]:st.ends[1]] == ref_first_six_tokens[1]
+    assert st.text[st.ends[1]:st.ends[2]] == ref_first_six_tokens[2]
+    assert st.text[st.ends[2]:st.ends[3]] == ref_first_six_tokens[3]
+    assert st.text[st.ends[3]:st.ends[4]] == ref_first_six_tokens[4]
+    assert st.text[st.ends[4]:st.ends[5]] == ref_first_six_tokens[5]
+    assert st.text[st.ends[-7]:st.ends[-6]] == ref_last_six_tokens[0]
+    assert st.text[st.ends[-6]:st.ends[-5]] == ref_last_six_tokens[1]
+    assert st.text[st.ends[-5]:st.ends[-4]] == ref_last_six_tokens[2]
+    assert st.text[st.ends[-4]:st.ends[-3]] == ref_last_six_tokens[3]
+    assert st.text[st.ends[-3]:st.ends[-2]] == ref_last_six_tokens[4]
+    assert st.text[st.ends[-2]:st.ends[-1]] == ref_last_six_tokens[5]
+    assert st.delta_mus.shape == ref_delta_mus.shape
+    assert ref_tolerances.shape == ref_delta_mus.shape
+
+
+def test_delta_mus():
+    st = Trajectory(ref_text)
+    st.calculate()
+    refdiffs = np.absolute(st.delta_mus - ref_delta_mus)
+    misfit_count = 0
+    worst_misfit = 0.
+    for t, _ in enumerate(ref_tolerances):
+        for feat, _ in enumerate(ref_tolerances[t]):
+            misfit = refdiffs[t][feat] / ref_tolerances[t][feat]
+            if misfit > worst_misfit:
+                worst_misfit = misfit
+            if misfit > 1:
+                misfit_count += 1
+    misfit_percentage = misfit_count * 100. / ref_tolerances.size
+    assert ref_criterion_embeddings_match(misfit_percentage, worst_misfit)
 
 
 def test_transitivity():
-    st = Trajectory(some_text)
+    st = Trajectory(ref_text)
     st.calculate()
-    sum_of_delta_mus = np.zeros(len(st.delta_mus[0]))
-    for delta_mu in st.delta_mus:
-        sum_of_delta_mus = sum_of_delta_mus + delta_mu
+    sum_of_delta_mus = np.sum(st.delta_mus, axis=0)
     resp = openai.Embedding.create(input=[st.text], engine=st._engine)
     mu_endstate = np.array(resp['data'][0]['embedding'])
-    assert len(mu_endstate) == 1536
-    assert np.allclose(sum_of_delta_mus, mu_endstate, rtol=1e-02)
+    assert len(mu_endstate) == len(ref_delta_mus[0])
+    diffs = np.absolute(mu_endstate - sum_of_delta_mus)
+    misfit_count = 0
+    worst_misfit = 0.
+    for feat, diff in enumerate(diffs):
+        misfit = diff / ref_tolerances[-1][feat]
+        if misfit > worst_misfit:
+            worst_misfit = misfit
+        if misfit > 1:
+            misfit_count += 1
+    misfit_percentage = misfit_count * 100. / len(diffs)
+    assert ref_criterion_embeddings_match(misfit_percentage, worst_misfit)
 
 
 def test_calculate_twice():
-    st = Trajectory(some_text)
+    st = Trajectory(ref_text)
     st.calculate()
     with pytest.raises(TrajectoryException):
         st.calculate()
@@ -95,7 +147,7 @@ def test_tokenizer_is_not_fast():
     # kind of a bogus test: will normally pass through a pytest run uselessly, but (1) will catch if pytest is somehow
     # run in an environment with a non-fast tokenizer, and (2) if provoked by momentarily changing the default
     # tokenizer to a non-fast one, can prove that the non-fast exception code can successfully do the raise.
-    st = Trajectory(some_text)
+    st = Trajectory(ref_text)
     if st._tokenizer.is_fast is False:
         with pytest.raises(TrajectoryException):
             st.calculate()
